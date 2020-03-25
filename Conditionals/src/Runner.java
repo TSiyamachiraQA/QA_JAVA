07f7fcfb1293b7a9ce0eaef36225725c0a4ad1e5
TSiyamachiraQA <QASiyamachira@gmail.com>
2020-03-25 06:32:29 +0000

    first commit

---
+++ Conditionals/src/Runner.java
@@ -0,0 +1,18 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		int firstnum = 1;
+		int secnum = 3;
+		boolean operator = true;
+		
+		if (operator == true) {
+			System.out.println(firstnum + secnum);
+		}
+		else if (operator == false) {
+			System.out.println(firstnum + secnum);
+		}
+
+	}
+
+}
